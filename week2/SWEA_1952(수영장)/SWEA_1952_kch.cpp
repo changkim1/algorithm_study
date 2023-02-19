bbea587cceda0396cabd993e13d9586bde076997
changkim <zzankor@naver.com>
2023-02-19 17:46:07 +0900

    kch_add_annotation

--- week2/SWEA_1952(수영장)/SWEA_1952_kch.cpp
+++ week2/SWEA_1952(수영장)/SWEA_1952_kch.cpp
@@ -5,13 +5,13 @@
 using namespace std;
 
 int t;
-int fee[4];
-int month_plan[13];
+int fee[4]; // 0 : 1일권 , 1 : 한달권, 2 : 3달권, 3 : 1년권
+int month_plan[13]; // 몇 번 갈지 저장하는 배열
 int ans;
 
-void	solve(int mon, int sum)
+void	solve(int mon, int sum) // mon = 해당 월에 fee를 path 처럼 저장하는 방식으로 (실제로는 해당 요금 지불 방식을 sum 에 더해주기만 함)
 {
-	if (mon > 12)
+	if (mon > 12) // 12월이 넘어가면 최솟값 갱신
 	{
 		ans = min(ans, sum);
 		// printf("solvesum = %d\n", ans);
@@ -21,21 +21,21 @@ void	solve(int mon, int sum)
 	{
 		if (i == 0)
 		{
-			sum += (fee[0] * month_plan[mon]);
-			solve(mon + 1, sum);
-			sum -= (fee[0] * month_plan[mon]);
+			sum += (fee[0] * month_plan[mon]); // 해당 mon 은 1일권을 산 것으로 하고, sum에 더해줌 (1일권 * 몇 번 갈지)
+			solve(mon + 1, sum); // 다음 달로 가기
+			sum -= (fee[0] * month_plan[mon]); // 빼주기
 		}
 		else if (i == 1)
 		{
-			sum += fee[1];
-			solve(mon + 1, sum);
-			sum -= fee[1];
+			sum += fee[1]; // 해당 mon 은 한달권을 산 것으로 하고, sum에 더해줌 (한달권 1개)
+			solve(mon + 1, sum); // 다음 달로 가기
+			sum -= fee[1]; // 빼주기
 		}
 		else if (i == 2)
 		{
-			sum += fee[2];
-			solve(mon + 3, sum);
-			sum -= fee[2];
+			sum += fee[2]; // 해당 mon 은 3달권을 산 것으로 하고, sum에 더해줌 (3달권 1개)
+			solve(mon + 3, sum); // 3달권을 산 것이기 때문에 3달 뒤로 넘어가서 계산. 11, 12월일 경우에도 +3달 해줄 수 있음.
+			sum -= fee[2]; // 빼주기
 		}
 	}
 }
@@ -47,13 +47,13 @@ int main(void)
 	{
 		ans = (int)21e8;
 		for (int i = 0; i < 4; i++)
-			cin >> fee[i];
+			cin >> fee[i]; // 요금 입력받기
 		for (int i = 1; i < 13; i++)
-			cin >> month_plan[i];
+			cin >> month_plan[i]; // 몇 번 갈지 입력받기
 		solve(1, 0);
 		// printf("sum = %d\n", ans);
-		ans = min(ans, fee[3]);
-		printf("#%d %d\n", a + 1, ans);
+		ans = min(ans, fee[3]); // 1일권, 한달권, 3달권을 사용했던 것들 중에 가장 싼 값과 1년치 이용권을 비교해서 갱신
+		printf("#%d %d\n", a + 1, ans); // 최솟값 출력ㄴ
 	}
 	
 }
--- week2/SWEA_4012(요리사)/SWEA_4012_kch.cpp
+++ week2/SWEA_4012(요리사)/SWEA_4012_kch.cpp
@@ -8,27 +8,27 @@
 using namespace std;
 
 int n, t;
-int dat[17];
+int dat[17]; // 요리팀 저장할 배열 0, 1로 팀 구분
 int ans = 21e8;
 
-int map[17][17];
+int map[17][17]; // 요리별 시너지 저장
 
 void	solve()
 {
-	int aval = 0;
-	int bval = 0;
-	for (int i = 0; i < n; i++)
+	int aval = 0; // a요리의 총 시너지 저장
+	int bval = 0; // b요리의 총 시너지 저장
+	for (int i = 0; i < n; i++) // i번 요리 기준
 	{
 		int val = 0;
 		for (int j = 0; j < n; j++)
 		{
-			if (dat[i] == dat[j])
-				val += map[i][j];
+			if (dat[i] == dat[j]) // 만약 i와 j가 같은 팀이라면
+				val += map[i][j]; // val에 추가해주기 -> 한바퀴만 돌아도 어차피 j번이 기준이 될 때 i가 추가될 거기 때문에 괜찮음.
 		}
 		if (!dat[i])
-			aval += val;
+			aval += val; // dat가 0이라면 a팀 시너지에 추가
 		else
-			bval += val;
+			bval += val; // 1이라면 b팀 시너지에 추가
 	}
 	int solve_res = abs(aval - bval);
 	// for (int i = 0; i < n; i++)
@@ -36,19 +36,19 @@ void	solve()
 	// cout << endl;
 	// printf("a : %d b : %d res : %d ans : %d\n", aval, bval, solve_res, ans);
 	if (ans > solve_res)
-		ans = solve_res;
+		ans = solve_res; // ans 갱신
 }
 
 void	make_team(int idx, int now)
 {
-	if (idx == n / 2)
+	if (idx == n / 2) // 팀의 숫자가 전체의 반이 되면
 	{
-		solve();
+		solve(); // 계산하는 함수로 넘어가기
 		return ;
 	}
 	for (int i = now + 1; i < n; i++)
 	{
-		dat[i] = 1;
+		dat[i] = 1; // dat에 1을 넣고 빼고 하는 식으로 팀을 구성하기
 		make_team(idx + 1, i);
 		dat[i] = 0;
 	}
@@ -59,8 +59,8 @@ int main()
 	cin >> t;
 	for (int z = 0; z < t; z++)
 	{
-		ans = 21e8;
-		memset(dat, 0, sizeof(dat));
+		ans = 21e8; // 최솟값 저장할 변수
+		memset(dat, 0, sizeof(dat)); 
 		cin >> n;
 		for (int i = 0; i < n; i++)
 		{
